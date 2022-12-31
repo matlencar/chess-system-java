bf98204f33110ea943180caf02cbdf2338c22512
matlencar <rm94642@fiap.com.br>
2022-12-31 08:44:34 -0300

    BoardException and defensive programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
 
