22279f500ac18dab0ca84cd3159da2690a0f750c
matlencar <rm94642@fiap.com.br>
2023-01-19 22:58:00 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -11,40 +13,41 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 
 		while (true) {
 			try {
-			UI.clearScreen();
-			UI.printMatch(chessMatch);
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPossition(sc);
-			
-			boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-			UI.clearScreen();
-			UI.printBoard(chessMatch.getPieces(), possibleMoves);
-			
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPossition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-			} 
-			catch (ChessException e) {
+				UI.clearScreen();
+				UI.printMatch(chessMatch, captured);
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPossition(sc);
+
+				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
+
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPossition(sc);
+
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+
+				if (captured != null) {
+					captured.add(capturedPiece);
+				}
+			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			}
-			catch (InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
 		}
-		
 
 	}
 
